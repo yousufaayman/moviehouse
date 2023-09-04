3ba53f0170e82903a61c5429437ddf192427c3ea
yousufaayman <yousufaymanh04@gmail.com>
2023-09-04 17:14:42 +0300

    Added layout for movie info pop up, added class to handle OMDB api

--- app/src/main/java/com/yousufaayman/moviehouse/OMDBAPIService.kt
+++ app/src/main/java/com/yousufaayman/moviehouse/OMDBAPIService.kt
@@ -1,30 +1,28 @@
 package com.yousufaayman.moviehouse
 
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-data class MovieResponse(
-    val results: List<TMDBMovieModelClass>
-)
-interface TMDBApiService {
-    @GET("movie/popular")
-    fun getPopularMovies(
-        @Query("api_key") apiKey: String,
-        @Query("page") page: Int
-    ): Call<MovieResponse>
+interface OMDBApiService {
+    @GET("/")
+    suspend fun getMovieInfo(
+        @Query("apikey") apiKey: String,
+        @Query("t") movieTitle: String
+    ): Response<OMDBMovieModelClass>
 }
-object TMDBApiClient {
-    private const val BASE_URL = "https://api.themoviedb.org/3/"
 
-    fun create(): TMDBApiService {
+object OMDBApiClient {
+    private const val BASE_URL = "http://www.omdbapi.com/"
+
+    fun create(): OMDBApiService {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
-        return retrofit.create(TMDBApiService::class.java)
+        return retrofit.create(OMDBApiService::class.java)
     }
 }
